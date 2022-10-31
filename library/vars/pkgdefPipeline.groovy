aa4c974b2c7af16dd2414c3c9d4bdedc9f7bd47f
amaksyuta <andreymaksuyta@gmail.com>
2022-10-31 15:25:37 -0400

    Update pipelene for main
    Signed-off-by: amaksyuta <andreymaksuyta@gmail.com>

--- library/vars/pkgdefPipeline.groovy
+++ library/vars/pkgdefPipeline.groovy
@@ -1,4 +1,5 @@
-def call(body) {
+def call(body) 
+{
     // evaluate the body block, and collect configuration into the object
     def pipelineParams= [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
